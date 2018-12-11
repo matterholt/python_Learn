ef111ebcf569e8fad35a5383e1672c80a2ee6c26
MAtterholt <matterholt84@gmail.com>
2018-12-10 23:28:21 -0500

    working with forms

--- flask_tut/v02_Flask/app.py
+++ flask_tut/v02_Flask/app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegisrationForm, LoginForm
 
-#
+
 app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = 'f3a37250268d0dddaf7db3f6480e08ff'
 
 # dummy post !list of dictonaries
 posts = [
@@ -32,6 +36,20 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html", title = 'aboutThis')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegisrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/Login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == "__main__":
